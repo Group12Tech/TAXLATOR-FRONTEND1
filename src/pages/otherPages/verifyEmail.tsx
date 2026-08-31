ae40990f0bddf6dfb9e47ab5b7e5a06d446da119
nwokoriechuks <113777505+nwokoriechuks@users.noreply.github.com>
2026-08-31 11:43:58 +0100

    all updates committed

--- src/pages/otherPages/verifyEmail.tsx
+++ src/pages/otherPages/verifyEmail.tsx
@@ -1,6 +1,6 @@
 // src/pages/otherPages/verifyEmail.tsx
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../state/useAuth";
 
